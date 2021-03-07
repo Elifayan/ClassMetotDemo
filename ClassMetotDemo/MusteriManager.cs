8e7bc64da590333d9120725834ff260b6ba2a191
Elif Ayan <77552697+Elifayan@users.noreply.github.com>
2021-03-07 18:46:39 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : "+ musteri.Id + "-" +musteri.Adi+ " "+ musteri.Soyadi);
+        }
+
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : "+ musteri.Id + "-" + musteri.Adi + " " + musteri.Soyadi);
+
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi : " + musteri.Id + "-" + musteri.Adi + " " + musteri.Soyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri() { };
+            musteri1.Id =1;
+            musteri1.Adi = "Elif";
+            musteri1.Soyadi = "Ayan";
+            musteri1.Limit = 15000;
+
+
+            Musteri musteri2 = new Musteri() { };
+            musteri2.Id = 2;
+            musteri2.Adi = "Engin";
+            musteri2.Soyadi = "Demiroğ";
+            musteri2.Limit = 20000;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Ayşe";
+            musteri3.Soyadi = "Yıldırm";
+            musteri3.Limit = 5000;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 4;
+            musteri4.Adi = "Ahmet";
+            musteri4.Soyadi = "Korkmaz";
+            musteri4.Limit = 1000;
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3,musteri4 } ;
+
+            //foreach (Musteri musteri in musteriler)
+            //{
+            //    Console.WriteLine(musteri.Id);
+            //    Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            //    Console.WriteLine(musteri.Limit);
+            //    Console.WriteLine("------------------------------------");
+            //}
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("---------------Müşteri Ekle-------------");
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+
+            Console.WriteLine("---------------Müşteri sil--------------");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri4);
+
+            Console.WriteLine("---------------Müşteri Listele--------------");
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+            musteriManager.Listele(musteri4);
+
+
+
+
+        }
+    }
+}
